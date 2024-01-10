60a09fa4734d7150eab5c97b17250e5b33687646
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2024-01-10 12:46:22 +0100

    Implement search by title

--- src/hooks/use-articles.ts
+++ src/hooks/use-articles.ts
@@ -1,20 +1,28 @@
+import { useState } from 'react';
 import { $http } from '@/lib/http';
 import { Article } from '@/types';
 import { useQuery } from '@tanstack/react-query';
 
 const useArticles = () => {
+    const [searchVal, setSearchVal] = useState('');
+
     const getArticles = async () => {
-        const { data } = await $http.get('/posts');
+        let params = {};
+        if (searchVal) {
+            params = { title: searchVal }
+        }
+
+        const { data } = await $http.get('/posts', {params});
     
         return data as Article[]
     }
 
     const { data, isLoading, refetch } = useQuery({
-        queryKey: ['articles'],
+        queryKey: ['articles', searchVal],
         queryFn: getArticles
     });
 
-    return { data, isLoading, refetch }
+    return { data, isLoading, refetch, setSearchVal, searchVal }
 }
 
 export default useArticles;
--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -2,25 +2,27 @@ import ArticleCard from '@/components/article/article'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import useArticles from '@/hooks/use-articles'
+import { useRef } from 'react'
 
 const ViewArticles = () => {
-  const { data, isLoading } = useArticles();
+  const { data, isLoading, setSearchVal, searchVal } = useArticles();
+  const searchRef = useRef<HTMLInputElement>(null)
 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(e);
-  }
-
-  if (isLoading) {
-    return <>Loading articles...</>
+    setSearchVal(searchRef.current!.value);
   }
 
   return (
     <>
       <form onSubmit={handleSearch} className='flex flex-row items-center gap-3'>
         <Input 
-          type="search" placeholder='Type author name or article title' 
+          type="search" 
+          name='search'
+          id="search"
+          placeholder='Type author name or article title' 
           className='p-3 border rounded-md my-2 w-[20rem]' 
+          ref={searchRef}
         />
 
         <Button 
@@ -32,8 +34,10 @@ const ViewArticles = () => {
         </Button>
       </form>
 
-
-      <div className='grid grid-cols-4 gap-4'>
+      <div className='grid grid-cols-2 gap-4'>
+        {
+          isLoading && <>Loading articles ...</>
+        }
         {
           data && data.map((article) => {
             return <ArticleCard article={article} />
