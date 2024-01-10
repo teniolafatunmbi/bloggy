bc9bc6d9c2c9369ecf83aac648b90a9781b11092
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2024-01-10 23:23:50 +0100

    Updated test for article card

--- src/__tests__/article.test.tsx
+++ src/__tests__/article.test.tsx
@@ -1,23 +1,52 @@
 import { expect, test } from 'vitest';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import ArticleCard from '@/components/article/article';
+import { ArticlesProvider } from '@/context';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { getUser } from '@/adapter/get-user';
 
-const article = {
-    title: 'Test article',
-    body: "Test article body",
-    userId: 1,
-    id: 1,
-}
 
-test("displays the article card with title and body", async () => {
-    const articleCard = render(<ArticleCard article={article} />);
+test("displays the article card with title, body, and author information", async () => {
+
+    const article = {
+        title: 'Test article',
+        body: "Test article body",
+        userId: 1,
+        id: 1,
+    }
+    const mockQueryClient = new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false
+          }
+        }
+    });
+
+    const mockUser = await getUser(article.userId);
+
+    const articleCard = render(
+                <>
+                <QueryClientProvider client={mockQueryClient}>
+                    <ArticlesProvider>
+                        <ArticleCard article={article} />
+                    </ArticlesProvider>
+                </QueryClientProvider>
+                </>
+        );
 
     const articleTitle = await articleCard.findByTestId('article-title');
     const articleContent = await articleCard.findByTestId('article-content');
-    // const articleCard = articleCard.findByTestId('article-card');
+    const articleAuthorName = await articleCard.findByTestId('article-author-name');
+    const articleAuthorEmail = await articleCard.findByTestId('article-author-email');
 
     expect(articleTitle.innerText).toBe(article.title)
-    expect(article.body).toContain(articleContent.innerText);
+    expect(articleContent.innerHTML).toContain(article.body);
+
+    await waitFor(() => {
+        expect(articleAuthorName.innerText).toBe(mockUser.name);
+        expect(articleAuthorEmail.innerText).toBe(mockUser.email)
+    });
 
     articleCard.unmount();
+
 })
