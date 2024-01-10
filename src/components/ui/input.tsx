ae1a2326046959aafbbc19ffd1db9fcdcce46801
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2024-01-10 14:32:22 +0100

    Add toast component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -7,18 +7,29 @@ const inputVariants = cva(
   "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
   )
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentPropsWithoutRef<'input'>>(
-  ({ className, type, ...props }, ref) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type InputProps = React.ComponentPropsWithoutRef<'input'> & { formik?: any }
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, formik, ...props }, ref) => {
+
     return (
-      <input
-        type={type}
-        className={cn(
-            inputVariants,
-          className
+      <>
+        <input
+          type={type}
+          className={cn(
+              inputVariants(),
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        {formik && (formik?.touched?.[props.name!] && formik?.errors?.[props.name!]) && (
+            <p className="m-0 text-rose-700" style={{fontSize: '13px'}}>
+              <small>{formik?.errors?.[props.name!] as unknown as string}</small>
+            </p>
         )}
-        ref={ref}
-        {...props}
-      />
+      </>
     )
   }
 )
--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { cn } from "@/lib"
  
 export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    formik?: any
+  }
  
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, ...props }, ref) => {
+  ({ className, formik, ...props }, ref) => {
     return (
-      <textarea
+      <>
+        <textarea
         className={cn(
           "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
           className
@@ -15,6 +21,14 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
         ref={ref}
         {...props}
       />
+
+      {formik && (formik?.touched?.[props.name!] && formik?.errors?.[props.name!]) && (
+        <p className="m-0 text-rose-700" style={{fontSize: '13px'}}>
+          <small>{formik?.errors?.[props.name!] as unknown as string}</small>
+        </p>
+    )}
+      </>
+      
     )
   }
 )
