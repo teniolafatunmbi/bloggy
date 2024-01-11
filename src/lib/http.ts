6ae6598d2dc1242d39ab3157b6e4ed583114ce1e
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2024-01-11 01:23:35 +0100

    Remove boilerplate on

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
 
 export const $http = axios.create({
-    headers: {
-      "Access-Control-Allow-Origin": import.meta.env.VITE_APP_URL,
-    },
     baseURL: "https://jsonplaceholder.typicode.com/",
 });
