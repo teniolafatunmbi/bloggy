1b24b670c12c1a2f1afe5632f3ed220c158c666a
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2024-01-11 01:24:18 +0100

    Remove unused code

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,14 +1,9 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import moment from "moment";
 
 export function cn(...inputs: ClassValue[]) {
    return twMerge(clsx(inputs));
 }
-
-export const formatDate = (i: string) => {
-   return moment(i).format("MMM Do YY");
-};
 
 export const truncateText = (str: string, limit: number) => {
    if (str.length > limit) {
